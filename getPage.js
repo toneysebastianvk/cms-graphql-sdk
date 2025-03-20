c8da84be8db1b46e5bc7030f3f13ecaed9121687
tsebastian <toney.sebastian@underarmour.com>
2025-03-20 23:42:21 +0530

    preview added

--- getPage.js
+++ getPage.js
@@ -1,32 +1,38 @@
-class getPage {
-  static pageQuery(deliveryKey) {
-    return `
-        query getAmpliencePage {
-        page(deliveryKey: "${deliveryKey}") {
-          slots {
-            __typename
-            ... on Heromini {
-              media {
-                id
-                url
-                __typename
-              }
-              rawJson {
-                content
-              }
-            }
-            ... on Carousel {
-              rawJson {
-                content
-              }
-            }
-          }
-        }
-      }
-      `;
+const {
+  ContentClient,
+  StagingEnvironmentFactory,
+} = require("dc-delivery-sdk-js");
+
+const HUB_NAME = "uasandbox";
+class AmplienceSDK {
+  client;
+  constructor() {}
+
+  async init(preview) {
+    if (!preview) {
+      this.client = new ContentClient({
+        hubName: HUB_NAME,
+      });
+    } else {
+      const factory = new StagingEnvironmentFactory(
+        "uowk0qxoku001ufatmmwkv1fe.staging.bigcontent.io"
+      );
+      const stagingEnvironmentAtTimestamp = await factory.generateDomain({
+        //timestamp: 1742495400000,
+      });
+      this.client = new ContentClient({
+        hubName: HUB_NAME,
+        stagingEnvironment: stagingEnvironmentAtTimestamp,
+      });
+    }
+  }
+
+  async getModule(deliveryKey) {
+    const content = await this.client.getContentItemByKey(deliveryKey);
+    return content.body;
   }
 }
 
 module.exports = {
-  getPage,
+  AmplienceSDK,
 };
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { ApolloServer, gql } = require("apollo-server");
-const { AmplienceGraphQLApi } = require("./api");
+const { AmplienceSDK } = require("./getpage");
 
 // Step 1: Define the GraphQL Schema
 
@@ -16,139 +16,7 @@ const typeDefs = gql`
   }
 
   type Page {
-    slots: [Slots]
-  }
-
-  union Slots = Carousel | Heromini | Banner | Contentcarousel | Slot
-
-  type Carousel {
-    rawJson: CarouselRawJson
-  }
-
-  type CarouselRawJson {
-    content: CarouselContent
-  }
-  type CarouselContent {
-    subheadlinePlainText: String
-    component: String
-    snipeText: String
-    _meta: Meta
-    items: [CarouselItem]
-    height: Int
-    headlineText: String
-  }
-  type CarouselItem {
-    items: [CarouselItemContent]
-    _meta: Meta
-  }
-  type CarouselItemContent {
-    headlineText: String
-    subheadlinePlainText: String
-    target: CarouselItemTarget
-    mediaType: String
-    mediaExternalId: String
-  }
-  type CarouselItemTarget {
-    targetType: String
-  }
-  type Meta {
-    schema: String
-    name: String
-    deliveryId: String
-  }
-  type Heromini {
-    media: HeroMedia
-    rawJson: HeroMiniRawJson
-  }
-
-  type HeroMedia {
-    id: String
-    url: String
-  }
-
-  type HeroMiniRawJson {
-    content: HeroMiniContent
-  }
-
-  type HeroMiniContent {
-    teasertargets: [TeaserTarget]
-    _meta: Meta
-    styles: HeroMiniStyles
-    media: Media
-    teaser: Teaser
-    isPersonalization: Boolean
-  }
-
-  type TeaserTarget {
-    _meta: Meta
-    items: [TeaserItem]
-  }
-
-  type TeaserItem {
-    headlineText: String
-    target: TeaserItemTarget
-  }
-
-  type TeaserItemTarget {
-    url: String
-  }
-
-  type Meta {
-    schema: String
-    name: String
-    deliveryKey: String
-    deliveryId: String
-  }
-
-  type HeroMiniStyles {
-    useTextLinks: Boolean
-    centerCopyText: Boolean
-    twoColumns: Boolean
-    linkedStyles: LinkedStyles
-    width: Int
-  }
-
-  type LinkedStyles {
-    desktopStyle: String
-    mobileStyle: String
-  }
-
-  type Media {
-    _meta: Meta
-    id: String
-    name: String
-    endpoint: String
-    defaultHost: String
-    mimeType: String
-  }
-
-  type Teaser {
-    headline: TeaserHeadline
-    subheadline: TeaserSubheadline
-    teaserTitle: String
-    snipeText: String
-    _meta: Meta
-  }
-
-  type TeaserHeadline {
-    teaserTitle: String
-    style: String
-  }
-
-  type TeaserSubheadline {
-    teaserTitle: String
-    style: String
-  }
-  type Banner {
-    rawJson: JSON
-  }
-
-  type Contentcarousel {
-    rawJson: JSON
-  }
-
-  type Slot {
-    rawJson: JSON
+    slots: JSON
   }
 `;
 
@@ -159,9 +27,10 @@ const resolvers = {
       return "Hello, world!";
     },
     async getAmplienceContent(_parent, args) {
-      const amplienceGraphQLApi = new AmplienceGraphQLApi(args.preview);
-      const result = await amplienceGraphQLApi.getModule(args.deliveryKey);
-      return result;
+      const amplienceInstance = new AmplienceSDK();
+      await amplienceInstance.init(args.preview);
+      const result = await amplienceInstance.getModule(args.deliveryKey);
+      return { page: { ...result } };
     },
   },
 };
@@ -171,10 +40,10 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   introspection: true,
-  playground: true, 
+  playground: true,
 });
 
 // Step 4: Launch the server
-server.listen({ port: 3000, path: '/graphql' }).then(({ url }) => {
+server.listen({ port: 3000, path: "/graphql" }).then(({ url }) => {
   console.log(`Server ready at ${url}graphql`);
 });
